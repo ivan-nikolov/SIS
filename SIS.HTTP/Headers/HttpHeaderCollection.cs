552d1a8949e0606a9ae9ad50f11d0bc8a68eb0b0
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-01 21:18:16 +0200

    Added HttpHeader and HttpHeaderCollection

---
+++ SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,47 @@
+﻿namespace SIS.HTTP.Headers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Common;
+    using Contracts;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void AddHeader(HttpHeader header)
+        {
+            CoreValidator.ThrowIfNull(header, nameof(HttpHeader));
+
+            this.headers[header.Key] = header;
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.headers.ContainsKey(key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            if (!this.headers.ContainsKey(key))
+            {
+                return null;
+            }
+
+            return this.headers[key];
+        }
+
+        public override string ToString() => string.Join(Environment.NewLine, this.headers.Select(v => v.Value.ToString()));
+    }
+}
