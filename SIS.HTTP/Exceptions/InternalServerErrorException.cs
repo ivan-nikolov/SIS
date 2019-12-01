5f8ed23bda86e4c17ddbf2898cbf9e0a74764dd6
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-01 17:27:38 +0200

    Added BadRequestException and InternalServerErrorException in Excepitons namspace

---
+++ SIS.HTTP/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,19 @@
+﻿namespace SIS.HTTP.Exceptions
+{
+    using System;
+
+    public class InternalServerErrorException : Exception
+    {
+        private const string DefaultMessage = "The Server has encountered an error.";
+
+        public InternalServerErrorException()
+            : this(DefaultMessage)
+        {
+        }
+
+        public InternalServerErrorException(string message)
+            : base(message)
+        {
+        }
+    }
+}
