442ec28e79f6b380d45097eb82a633f62e17edd9
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-01 17:34:49 +0200

    Added StringExtensions in Extensions namespace

---
+++ SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using SIS.HTTP.Common;
+
+namespace SIS.HTTP.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string text) => char.ToUpper(text[0]) + text.Substring(1).ToLower();
+    }
+}
