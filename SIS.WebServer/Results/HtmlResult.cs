d3bc2105033024c9ac2198e7e32442f2bf8a0b2b
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-17 16:12:51 +0200

    HtmlResult Added

---
+++ SIS.WebServer/Results/HtmlResult.cs
@@ -0,0 +1,21 @@
+﻿namespace SIS.WebServer.Results
+{
+    using System.Text;
+
+    using HTTP.Enums;
+    using HTTP.Headers;
+    using HTTP.Responses;
+
+    public class HtmlResult : HttpResponse
+    {
+        private const string DefaultContentType = "text/html; charset=utf-8";
+        private const string HttpHeaderKey = "Content-Type";
+
+        public HtmlResult(string content, HttpResponseStatusCode responseStatusCode)
+            : base(responseStatusCode)
+        {
+            this.Headers.AddHeader(new HttpHeader(HttpHeaderKey, DefaultContentType));
+            this.Content = Encoding.UTF8.GetBytes(content);
+        }
+    }
+}
