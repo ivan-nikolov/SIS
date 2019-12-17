819c94d630d3482c0a05829894171154b91f7725
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-17 17:21:03 +0200

    Server Added

---
+++ SIS.WebServer/Server.cs
@@ -0,0 +1,51 @@
+﻿namespace SIS.WebServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using SIS.WebServer.Routing.Contracts;
+
+    public class Server
+    {
+        private const string LocalHostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+
+        private TcpListener listener;
+
+        private readonly IServerRoutingTable serverRoutingTable;
+
+        private bool isRunning;
+
+        public Server(int port, IServerRoutingTable serverRoutingTable)
+        {
+            this.port = port;
+            this.listener = new TcpListener(IPAddress.Parse(LocalHostIpAddress), port);
+
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public void Run()
+        {
+            this.listener.Start();
+            this.isRunning = true;
+
+            while (this.isRunning)
+            {
+                Console.WriteLine("Waiting for client...");
+
+                var client = this.listener.AcceptSocket();
+
+                this.Listen(client);
+            }
+        }
+
+        public async Task Listen(Socket client)
+        {
+            var connectionHandler = new ConnectionHandler(client, this.serverRoutingTable);
+
+            connectionHandler.ProcessRequest();
+        }
+    }
+}
