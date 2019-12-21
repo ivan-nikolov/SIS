4a7777493e137135658c11f77a03672a2477064a
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-21 13:31:56 +0200

    HttpCookieCollection Added

---
+++ SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,52 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Common;
+    using Cookies.Contracts;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private readonly Dictionary<string, HttpCookie> httpCookies;
+
+        public HttpCookieCollection()
+        {
+            this.httpCookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            this.httpCookies.Add(cookie.Key, cookie);
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.httpCookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.httpCookies[key];
+        }
+
+        public bool HasCookies()
+        {
+            return this.httpCookies.Count != 0;
+        }
+
+        public IEnumerator<HttpCookie> GetEnumerator()
+        {
+            return this.httpCookies.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
