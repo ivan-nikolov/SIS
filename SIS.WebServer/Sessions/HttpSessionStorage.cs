7faa20aad17219ab95a2ddfdc37269129abcb396
ivan-nikolov <i.nikolov.iliev@gmail.com>
2019-12-21 15:31:15 +0200

    Server HttpSessionStorage Added

---
+++ SIS.WebServer/Sessions/HttpSessionStorage.cs
@@ -0,0 +1,21 @@
+﻿namespace SIS.WebServer.Sessions
+{
+    using System.Collections.Concurrent;
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Sessions;
+    using SIS.HTTP.Sessions.Contracts;
+
+    public class HttpSessionStorage
+    {
+        public const string SessionCookieKey = "SIS_ID";
+
+        private static readonly ConcurrentDictionary<string, IHttpSession> sessions = new ConcurrentDictionary<string, IHttpSession>();
+
+        public static IHttpSession GetSession(string id)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(id, nameof(id));
+
+            return sessions.GetOrAdd(id, _ => new HttpSession(id));
+        }
+    }
+}
